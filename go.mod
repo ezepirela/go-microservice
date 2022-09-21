8b7e09eedaf944da0ca7c3af47b73fba4ae6d37d
ezepirela <ezepirela@gmail.com>
2022-09-21 15:33:33 -0300

    feat first functional server

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ezepirela/go-microservice
+
+go 1.19
---
+++ main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/ezepirela/go-microservice/handlers"
+)
+
+func main() {
+
+	l := log.New(os.Stdout, "product-api", log.LstdFlags)
+	hh := handlers.NewHello(l)
+	gh := handlers.NewGoodbye(l)
+
+	sm := http.NewServeMux()
+	sm.Handle("/", hh)
+	sm.Handle("/goodbye", gh)
+
+	s := &http.Server{
+		Addr:         ":9090",
+		Handler:      sm,
+		IdleTimeout:  120 * time.Second,
+		ReadTimeout:  1 * time.Second,
+		WriteTimeout: 1 * time.Second,
+	}
+
+	go func() {
+		err := s.ListenAndServe()
+		if err != nil {
+			l.Fatal(err)
+		}
+	}()
+
+	signChannel := make(chan os.Signal)
+	signal.Notify(signChannel, os.Interrupt)
+	signal.Notify(signChannel, os.Kill)
+
+	sig := <-signChannel
+	l.Println("Received terminate, graceful shutdown", sig)
+
+	tc, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	s.Shutdown(tc)
+}
